bbb246071111beca75eb2e6d0e3640f7c90493e5
Laura Visockaite <viso.laura151@gmail.com>
2019-01-12 12:37:53 +0000

    Changes to some text and added work section

--- js/script.js
+++ js/script.js
@@ -54,6 +54,25 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
+var acc = document.getElementsByClassName("accordion3");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
     scrollFunction()
